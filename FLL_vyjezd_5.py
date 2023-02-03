9e4ed575a3b401d32ecefef5cf9b039152a8442e
Adry36 <47398761+Adry36@users.noreply.github.com>
2023-02-03 17:21:12 +0100

    ok

--- FLL_vyjezd_5.py
+++ FLL_vyjezd_5.py
@@ -2,6 +2,7 @@
 
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
+from spike.operator import less_than
 from math import *
 from math import pi
 
@@ -71,37 +72,37 @@ def gyro_steer_l(negativni_zatacka, levy, pravy):
     mot.stop()
 
 def zarovnani_l(rychlost_leva, rychlost_prava):
-    while cl.get_reflected_light()>cerna_zarovnani:
+    while cl.get_reflected_light()>=cerna_zarovnani:
         mot.start_tank_at_power(rychlost_leva, rychlost_prava)
     mot.stop()
-    while cr.get_reflected_light()>stred:
+    while cr.get_reflected_light()>=stred:
         mot.start_tank_at_power(0, rychlost_prava)
     mot.stop()
-    while cl.get_reflected_light()<cerna_zarovnani:
-        mot.start_tank_at_power(-15, 0)
+    while cl.get_reflected_light()<=cerna_zarovnani:
+        mot.start_tank_at_power(-25, 0)
     mot.stop()
-    while cl.get_reflected_light()<stred:
-        mot.start_tank_at_power(-10, 0)
+    while cl.get_reflected_light()<=stred:
+        mot.start_tank_at_power(-25, 0)
     mot.stop()
-    while cr.get_reflected_light()<stred:
-        mot.start_tank_at_power(0, -15)
+    while cr.get_reflected_light()<=stred:
+        mot.start_tank_at_power(0, -25)
     mot.stop()
 
 def zarovnani_r(rychlost_leva, rychlost_prava):
-    while cr.get_reflected_light()>cerna_zarovnani:
+    while cr.get_reflected_light()>=cerna_zarovnani:
         mot.start_tank_at_power(rychlost_leva, rychlost_prava)
     mot.stop()
-    while cl.get_reflected_light()>stred:
+    while cl.get_reflected_light()>=stred:
         mot.start_tank_at_power(rychlost_leva, 0)
     mot.stop()
-    while cr.get_reflected_light()<cerna_zarovnani:
-        mot.start_tank_at_power(0, -15)
+    while cr.get_reflected_light()<=cerna_zarovnani:
+        mot.start_tank_at_power(0, -25)
     mot.stop()
-    while cr.get_reflected_light()<stred:
-        mot.start_tank_at_power(0, -10)
+    while cr.get_reflected_light()<=stred:
+        mot.start_tank_at_power(0, -25)
     mot.stop()
-    while cl.get_reflected_light()<stred:
-        mot.start_tank_at_power(-15, 0)
+    while cl.get_reflected_light()<=stred:
+        mot.start_tank_at_power(-25, 0)
     mot.stop()
 
 def jizda_po_care(jak_daleko, jak_rychle = 30, jaky_senzor = "r", strana = "r", kp = 0.075, ki = 0.001, kd = 0.1):
@@ -139,17 +140,83 @@ def jizda_po_care(jak_daleko, jak_rychle = 30, jaky_senzor = "r", strana = "r", 
 
 hub.motion_sensor.reset_yaw_angle()
 
-move_gyro(600, 0, 50)
+move_gyro(650, 0, 50)
+wait_for_seconds(0.3)
 gyro_steer_l(-40, 0, 50)
+
+move_gyro(500, 0, 50)
+vzv.run_for_degrees(360, 100)
+#wait_for_seconds(0.3)
+vzv.run_for_degrees(350, -100)
+move_gyro(350, 0, 50)
+vzv.run_for_degrees(330, 100)
+gyro_steer_l(-15, -30, 30)
+move_gyro(590, 0, 50)
+gyro_steer_l(-28, -40, 0)
+mot.start_tank(40, 40)
+wait_until(cl.get_reflected_light, less_than, cerna_zarovnani)
+mot.stop()
+#zarovnani_l(30, 30)
+move_gyro(150, 0, 50)
+vzv.run_for_degrees(340, -100)
+move_gyro(-80, 0, -50, "vetsi")
 wait_for_seconds(0.3)
-move_gyro(850, 0, 50)
-wait_for_seconds(0.3)
-move_gyro(-150, 0, -30, "vetsi")
-vzv.run_for_degrees(360, 30)
-move_gyro(-50, 0, -30, "vetsi")
+vzv.run_for_degrees(340, 100)
+move_gyro(-180, 0, -50, "vetsi")
+gyro_steer_r(30, 30, -30)
+vzv.run_for_degrees(340, -100)
+move_gyro(400, 0, 50)
+rad.run_for_seconds(0.7, -50)
+wait_for_seconds(0.5)
+rad.run_for_seconds(0.5, 100)
+#gyro_steer_l(-10, -30, 30)
+move_gyro(120, 0, 50)
+vzv.run_for_seconds(0.9, 100)
 
 
 
 
+#zarovnani_l(30, 30)
+#move_gyro(200, 0, 50)
+#vzv.run_for_degrees(340, -100)
+#move_gyro(-130, 0, -50, "vetsi")
+##vzv.run_for_degrees(330, 100)
+##move_gyro(-100, 0, -50, "vetsi")
+#vzv.run_for_degrees(330, -100)
+#gyro_steer_r(45, 30, -30)
+
+
+
+
+
+#wait_for_seconds(0.3)
+#move_gyro(-150, 0, -30, "vetsi")
+#vzv.run_for_degrees(380, 30)
+#mot.move_tank(230, "degrees", -40, -35)
+#vzv.run_for_degrees(120, -100)
+#wait_for_seconds(0.3)
+#move_gyro(250, 0, 30)
+
+
+
+
+
+
+
+#move_gyro(-180, 0, -30, "vetsi")
+#vzv.run_for_degrees(100, -100)
+#move_gyro(300, 0, 50)
+
+#vzv.run_for_degrees(280, -100)
+#move_gyro(400, 0, 50)
+
+#move_gyro(150, 0, 50)
+
+#gyro_steer_l(-20, 0, 50)
+
+#mot.start_tank(50, 50)
+#wait_until(cl.get_reflected_light, less_than, cerna_zarovnani)
+#mot.stop()
+#gyro_steer_l(-40, -30, 30)
 
 
